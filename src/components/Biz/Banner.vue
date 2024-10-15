e9055b7a10a1809fb2b9bfb7d981694c076599ab
Shawn <shawn@ShawndeMacBook-Pro.local>
2024-10-15 13:37:07 +0800

    fix:banner

--- src/components/Biz/Banner.vue
+++ src/components/Biz/Banner.vue
@@ -13,10 +13,12 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
-// pc
+/* PC 端 */
 .Banner {
   width: 100%;
-  height: 100vh; /* PC端使用视口高度，动态调整 */
+  height: calc(
+    100vh + 2rem
+  ); /* 使用视口高度减去一定的偏移量（避免图片被裁剪） */
   .banner_img {
     width: 100%;
     height: 100%;
@@ -26,19 +28,30 @@ export default defineComponent({
     background-size: cover; /* 填满容器，不留空白 */
   }
 }
-// ipad
+
+/* ipad */
 @media screen and (max-width: 1200px) {
   .Banner {
-    height: 42.1875rem;
+    height: 42.1875rem; /* 保持iPad下的高度 */
   }
 }
-// mobile
+
+/* mobile */
 @media screen and (max-width: 750px) {
   .Banner {
-    height: 83rem;
+    // height: 83rem;
+    height: 61rem;
     .banner_img {
-      background-image: url('@/assets/banner_m.jpg');
+      background-image: url('@/assets/banner_m.jpg'); /* 切换为移动端图片 */
+      background-size: contain; /* 移动端图片使用 contain 以完整显示 */
     }
+  }
+}
+
+/* 针对大屏幕（宽度大于1600px的PC） */
+@media screen and (min-width: 1600px) {
+  .Banner {
+    height: calc(100vh + 8rem); /* 适应更大屏幕，减去一定的高度来防止裁剪 */
   }
 }
 </style>
