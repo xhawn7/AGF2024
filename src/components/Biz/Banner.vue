6b285ae2e315d8da43b45e9482a27f2915cd8444
Shawn <shawn@ShawndeMacBook-Pro.local>
2024-10-15 15:52:03 +0800

    fix:banner

--- src/components/Biz/Banner.vue
+++ src/components/Biz/Banner.vue
@@ -16,34 +16,30 @@ export default defineComponent({
 /* PC 端 */
 .Banner {
   width: 100%;
-  height: calc(
-    100vh + 2rem
-  ); /* 使用视口高度减去一定的偏移量（避免图片被裁剪） */
   .banner_img {
     width: 100%;
-    height: 100%;
+    height: 100vh; /* 保证图片高度为视口高度 */
     background-image: url('@/assets/banner_p.jpg');
-    background-position: center center;
+    background-position: top center;
     background-repeat: no-repeat;
-    background-size: cover; /* 填满容器，不留空白 */
+    background-size: contain; /* 等比缩放图片，保持宽度与显示器一致，且图片不会被拉伸 */
   }
 }
 
 /* ipad */
 @media screen and (max-width: 1200px) {
   .Banner {
-    height: 42.1875rem; /* 保持iPad下的高度 */
+    height: 42.1875rem; /* 保持等比例缩放 */
   }
 }
 
 /* mobile */
 @media screen and (max-width: 750px) {
   .Banner {
-    // height: 83rem;
     height: 61rem;
     .banner_img {
       background-image: url('@/assets/banner_m.jpg'); /* 切换为移动端图片 */
-      background-size: contain; /* 移动端图片使用 contain 以完整显示 */
+      background-size: contain; /* 使用contain，图片等比缩放 */
     }
   }
 }
@@ -51,7 +47,7 @@ export default defineComponent({
 /* 针对大屏幕（宽度大于1600px的PC） */
 @media screen and (min-width: 1600px) {
   .Banner {
-    height: calc(100vh + 8rem); /* 适应更大屏幕，减去一定的高度来防止裁剪 */
+    height: auto;
   }
 }
 </style>
